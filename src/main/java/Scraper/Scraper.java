533a00509a80aa0b86c74c42b36698c406004d25
mgurga <motiurga@gmail.com>
2021-04-17 19:35:23 -0700

    made test pass with selenium

--- src/main/java/Scraper/Scraper.java
+++ src/main/java/Scraper/Scraper.java
@@ -45,7 +45,7 @@ public class Scraper {
 //		options.addArguments("headless");
 		options.addArguments("mute-audio");
 		options.addArguments("disable-gpu");
-		options.addArguments("window-size=1280,720");
+		options.addArguments("window-size=1000,720");
 		options.addArguments("ignore-certificate-errors");
 		options.addArguments("allow-running-insecure-content");
 		options.addArguments("disable-dev-shm-usage");
@@ -54,14 +54,13 @@ public class Scraper {
 		WebDriverWait wait = new WebDriverWait(driver, 20);
 		driver.get(twitterurl);
 		
-		WebElement tweetelement = wait.until(ExpectedConditions.presenceOfElementLocated(By.tagName("input")));
-		tweetelement = tweetelement.findElement(By.xpath("//article/div/div/div"));
+		WebElement tweetelement = wait.until(ExpectedConditions.presenceOfElementLocated(By.tagName("article")));
+		String baseXpath = "//article/div/div/div";
 		
 //		WebClient wc = new WebClient(BrowserVersion.CHROME);
 //		wc.getOptions().setThrowExceptionOnScriptError(false);
 //		wc.getOptions().setJavaScriptEnabled(true);
 //		HtmlPage page = wc.getPage(twitterurl);
-//		String baseXpath = "//article/div/div/div";
 //		
 //		wc.waitForBackgroundJavaScriptStartingBefore(10000);
 //		
@@ -69,11 +68,17 @@ public class Scraper {
 //		String tweetretweets = page.getFirstByXPath(baseXpath + "/div[last()]/div[last()-1]/div//span");
 //		String tweetlikes = page.getFirstByXPath("(" + baseXpath + "/div[last()]/div[last()-1]/div//span[@style])[3]");
 //		
-//		out.setContent(parser.parseTweet(tweetcontent));
-//		out.setRetweets(parser.parseStr(tweetretweets));
-//		out.setLikes(parser.parseStr(tweetlikes));
-//		
+		out.setContent(parser.parseTweet(
+				tweetelement.findElement(By.xpath(baseXpath + "/div[last()]/div/div")).getText()));
+		out.setRetweets(parser.parseStr(
+				tweetelement.findElement(By.xpath(baseXpath + "/div[last()]/div[last()-1]/div//span")).getText()));
+		out.setLikes(parser.parseStr(
+				tweetelement.findElement(By.xpath("(" + baseXpath + "/div[last()]/div[last()-1]/div//span[@style])[3]")).getText()));
+
 //		wc.close();
+		
+		driver.close();
+		driver.quit();
 		
 		return out;
 	}

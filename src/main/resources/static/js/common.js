115f03c149de8b96f0d3f6451de9dd4e61d3f1f1
mgurga <motiurga@gmail.com>
2021-06-07 12:16:37 -0700

    added custom account bar next to favorite account bar
    also made small tweets have light font weight

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,9 +1,58 @@
-
 function convertTime(isodate) {
     var d = new Date()
     var utcdate = new Date(isodate.substring(0,10) + " " + isodate.substring(11, 16))
     var correctdate = new Date(utcdate.getTime() - (d.getTimezoneOffset()*60*1000));
     return correctdate.toLocaleString();
+}
+
+function addAccount() {
+    var acc = prompt("Input account handle, leave blank to cancel");
+    if(acc == "")
+        return;
+    var newCA;
+    if(localStorage.getItem("customaccounts") == null)
+        newCA = acc;
+    else
+        newCA = localStorage.getItem("customaccounts") + "," + acc;
+    localStorage.setItem("customaccounts", newCA);
+    createAccountButtons();
+}
+
+function removeAccount(acchandle) {
+    var cas = localStorage.getItem("customaccounts").split(",");
+    var out = "";
+    for(let ca of cas)
+        if(!(ca == acchandle))
+            out += ca + ",";
+    out = out.substring(0, out.length - 1);
+    localStorage.setItem("customaccounts", out);
+    createAccountButtons();
+}
+
+function createAccountButtons() {
+    var cabar = document.getElementById("cabar");
+    cabar.innerHTML = "";
+    var cas = localStorage.getItem("customaccounts");
+    if(cas == null)
+        return;
+    cas = cas.split(",");
+    for(let ca of cas) {
+        var opening = document.createElement("p");
+        var ending = document.createElement("p");
+        opening.innerHTML = "[";
+        ending.innerHTML = "] ";
+        cabar.append(opening);
+        var acclink = document.createElement("a");
+        acclink.className = "favoriteaccount";
+        acclink.innerHTML = ca;
+        acclink.href = "/" + ca;
+        cabar.append(acclink);
+        var deletebtn = document.createElement("button");
+        deletebtn.innerHTML = "-";
+        deletebtn.addEventListener("click", function() {removeAccount(ca)});
+        cabar.append(deletebtn);
+        cabar.append(ending);
+    }
 }
 
 for(let post of document.getElementsByClassName("postdate")) {
@@ -15,3 +64,5 @@ for(let num of document.getElementsByClassName("largenum")) {
     var compactNum = Intl.NumberFormat('en', { notation: 'compact' }).format(num.innerHTML);
     num.innerHTML = compactNum;
 }
+
+createAccountButtons();
